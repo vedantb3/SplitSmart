7e2930d4908ba18cd753ed48dc1156664ab9b13e
vedantb3 <bansalvedant3@gmail.com>
2024-05-16 18:03:18 +0530

    Add devise and users table

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string
+#  last_name              :string
+#  phone_number           :string
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #Association
+  has_many :group_memberships
+  has_many :groups, through: :group_memberships
+  has_many :expenses
+  has_many :expense_shares
+
+  # Friendships associations
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  # Inverse friendships to manage the bi-directional friendship relationship
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  # Validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+end
